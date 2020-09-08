9c380d01486e40f44ccb700147519f86d5c9cca9
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-08 14:11:00 -0700

    got character counter working and added commnents on top of composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,38 @@
+/* global document */
+/* eslint-env jquery */
+
+$(document).ready(() => {
+  //using keyup to immediately get the value in the textarea
+  $("#tweet-text").on('input', function() {
+    // console.log(charLength);
+    //charCount is the length of chars inputted by user on textarea
+    let charCount = $(this).val().length;
+    //our char limit is 140
+    let charLimit = 140;
+
+    // console.log(this).children("div")
+
+    //counter should be normal colour under 140 chars
+    if (charCount <= charLimit) {
+      $(this)
+        //For each element in the tree, get the first element that matches the selector by testing the element itself and traversing up through its ancestors in the DOM tree.
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass('.limit-reached')
+        .text(charLimit - charCount);
+    } else {
+      //counter should be red above 140 characters
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .addClass('.limit-reached')
+        .text(charLimit - charCount);
+    }
+  });
+
+
+
+
+
+
+});
